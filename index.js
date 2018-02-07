ac187db91b84fb9139da883f9b3383171059cdee
Akshita <aggarwalakshita1@gmail.com>
2018-02-07 23:52:22 +0530

    probot-app

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = robot => {
+	 robot.log('Yay, the app was loaded!')
+  robot.on('issues.opened', async context => {
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
+    const params = context.issue({body: 'Hello World!'})
+
+    // Post a comment on the issue
+    return context.github.issues.createComment(params)
+  })
+}
